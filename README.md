dbef73c23ec27a3516ce0f6b627a7d278cc7b88e
hagyeong-gwon <silk3278@gmail.com>
2023-01-13 00:10:07 +0900

    ADD: kafka.js process kill 시 kafka disconnect

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KafkaJS
 JS로 Kafka사용하기
+
+사용 npm : kafkajs
+
+참고 코드: https://kafka.js.org/docs/getting-started
--- useExample.js
+++ useExample.js
@@ -34,3 +34,31 @@ const useConsumer = async () => {
    *   });
    */
 };
+
+const errorTypes = ['unhandledRejection', 'uncaughtException'];
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
+
+// windows 에서는 uncaughtException 이 발생하면 아래 코드의 완료까지 기다려준다.
+errorTypes.forEach(type => {
+  process.on(type, async () => {
+    try {
+      console.log(`process.on ${type}`);
+      await consumer.consumer.disconnect();
+      process.exit(0);
+    } catch (e) {
+      console.error(type, e.message);
+      process.exit(1);
+    }
+  });
+});
+
+// pm2 stop, restart, reload, delete 시에는 SIGINT 가 온다.
+signalTraps.forEach(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.consumer.disconnect();
+    } finally {
+      process.kill(process.pid, type);
+    }
+  });
+});
